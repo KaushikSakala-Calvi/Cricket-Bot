b35d077ed2aaef516ea0821213414a08ec7008bd
Avipsa Panda <a.panda@devon.nl>
2022-06-03 15:46:46 +0530

    batting config model

---
+++ TestBot/TestBot/Model/BattingConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestBot.Model
+{
+    public class BattingConfig
+    {
+        public int Id { get; set; }
+        public string BowlingType { get; set; }
+        public string BowlerType { get; set; }
+        public string BowlSpeed { get; set; }
+        public string BowlingZone { get; set; }
+        public string SelectedShot { get; set; }
+    }
+}
