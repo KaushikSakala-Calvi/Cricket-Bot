f3a124dc7ce8a3ee2c9543d3f4754905b77cabb7
Karthik Vankayalapati <v.karthik@devon.nl>
2022-06-07 19:55:44 +0530

    updated the fielding matrix to new class

--- TestBot/TestBot/Matrix/FieldingMatrix.cs
+++ TestBot/TestBot/Matrix/FieldingMatrix.cs
@@ -12,68 +12,72 @@ namespace TestBot.Matrix
             var result = new List<FieldingModel>();
 
             #region RAF
+
             var RAFfieldingPositions = new List<FieldingModel>();
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Coverdrive });
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.latecut });
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Ondrive });
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.pull });
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Sweep });
-            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.hook });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.latecut });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Cut });
             RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.squarecut });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Squaredrive });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.Coverdrive });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
             RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Ondrive });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.hook });
+            RAFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.pull });
+
             #endregion
 
             #region RAFM
+
             var RAMFfieldingPositions = new List<FieldingModel>();
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Coverdrive });
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.latecut });
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Ondrive });
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.pull });
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Sweep });
-            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.hook });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.latecut });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Cut });
             RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.squarecut });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Squaredrive });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.Coverdrive });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
             RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Ondrive });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.hook });
+            RAMFfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.pull });
+
             #endregion
 
             #region RAS
             var RASfieldingPositions = new List<FieldingModel>();
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Coverdrive });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Coverdrive });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Ondrive });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.pull });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Offdrive });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Sweep });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.hook });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.latecut });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Cut });
             RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.squarecut });
-            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Ondrive });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Squaredrive });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.Coverdrive });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Ondrive });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.hook });
+            RASfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.pull });
             #endregion
 
             #region OB
             var OBfieldingPositions = new List<FieldingModel>();
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Coverdrive });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Coverdrive });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Ondrive });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.pull });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Offdrive });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Sweep });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.hook });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.latecut });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Cut });
             OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.squarecut });
-            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Ondrive });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Squaredrive });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.hook });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Ondrive });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Sweep });
+            OBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.pull });
             #endregion
 
             #region LB
             var LBfieldingPositions = new List<FieldingModel>();
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Coverdrive });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Coverdrive });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.hook });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.pull });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Offdrive });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.latecut });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.hook });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.latecut });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Cut });
             LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.squarecut });
-            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z1, Prvshot = Shots.Ondrive });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Squaredrive });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z3, Prvshot = Shots.Coverdrive });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Offdrive });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z2, Prvshot = Shots.Ondrive });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.Sweep });
+            LBfieldingPositions.Add(new FieldingModel() { fp = fieldPosition.z4, Prvshot = Shots.pull });
             #endregion
 
             switch (bowlerType)
